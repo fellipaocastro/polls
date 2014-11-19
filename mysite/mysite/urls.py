21129b34043827429199f9f80b0224f32591fcbf
Fellipe Castro <fellipe.castro@hotelurbano.com.br>
2014-11-19 10:23:53 -0200

    Write more views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),)
+    url(r'^admin/', include(admin.site.urls)),
+)
