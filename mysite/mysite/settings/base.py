57458d6acaeef459904f30a4eea791e35af5730d
Fellipe Castro <contact@fellipecastro.com>
2014-11-22 23:33:02 -0200

    Set LANGUAGE_CODE to 'pt-br'

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-au'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
